9ed823dcc6139bf15f5c5dba3f3b2234e41343fa
Tanvirgeek <tanvirgeek@gmail.com>
2025-06-07 07:03:40 +0600

    02-customization

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,18 @@
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
-  return <Button>Hello World</Button>;
+  return (
+    <div className="flex flex-col gap-10">
+      <div>
+        <Button variant={"elevated"}>I am a Button</Button>
+      </div>
+
+      <Input placeholder="I am an input" />
+      <Progress value={30} />
+      <Textarea placeholder="I am a text area" />
+    </div>
+  );
 }
